f5df57379bced67a40a04097351986302ec8aba0
Vaishnavi Zavar <zavarvaishnavi08@gmail.com>
2026-09-26 01:15:39 +0530

    Update home route response to version 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def home(): return "CI/CD Pipeline Version 1"
+def home(): return "CI/CD Pipeline Version 2"
 if __name__ == "__main__": app.run("0.0.0.0", 5000)
